52ed6a1c51b730f449190b387d510d141c137e1d
FRANCISCO JAVIER GONZÁLEZ URIBE <fgonzalezu@intranet.ucsh.cl>
2024-12-03 17:40:54 -0300

    arreglos del sistema

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
 alert ("Bienvenido al sitio web con javaScript");
+
+
+// Utilización de console.log para inspeccionar diferentes secciones del proyecto
+let cabecera = document.getElementById("encabezado");
+console.log (cabecera); 
+
+let logo = document.getElementById("corporativo");
+console.log (logo); 
+
+let proposito = document.getElementById("mision-vision");
+console.log (cabecera); 
+
+let medicos = document.getElementById("consultas");
+console.log (medicos); 
+
+let opiniones = document.getElementById("testimonio");
+console.log (opiniones); 
+
+let footer = document.getElementById("piepagina");
+console.log (footer); 
